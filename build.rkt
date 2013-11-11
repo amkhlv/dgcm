b2b1846a7e71217255eb98702db707bcf8091244
Andrei Mikhailov <amkhlv@gmail.com>
2013-11-11 16:49:43 -0200

    added cleanup and build scripts

--- build.rkt
+++ build.rkt
@@ -1,1 +1,17 @@
-/home/andrei/usr/lib/racket/utilities/build.rkt
+#!/usr/bin/racket
+
+#lang at-exp racket
+
+(require (planet esilkensen/yaml) racket/system)
+
+(define conf (call-with-input-file (string->path "REGISTRY.yaml") read-yaml))
+
+(when (hash-has-key? conf "html")
+  (for/list ([f (hash-ref conf "html")])
+    (if (hash-has-key? f "dest")
+        (system (string-append "scribble --dest " (hash-ref f "dest") " " (hash-ref f "name") ".scrbl"))
+        (system (string-append "scribble " (hash-ref f "name") ".scrbl")))))
+
+(when (hash-has-key? conf "htmls")
+  (for/list ([f (hash-ref conf "htmls")])
+    (system (string-append "scribble --htmls " f ".scrbl"))))
